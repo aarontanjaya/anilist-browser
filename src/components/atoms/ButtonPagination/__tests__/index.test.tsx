c6bf49c9020cde57207681a0d1889473f56d2b4a
aaron tanjaya <aaront.tjy@gmail.com>
2023-11-05 22:13:01 +0700

    feat: setup jest, rtl, add initial testing for ButtonPagination

---
+++ src/components/atoms/ButtonPagination/__tests__/index.test.tsx
@@ -0,0 +1,8 @@
+import { render } from '@testing-library/react';
+import ButtonPagination from '..';
+
+describe('test ButtonPagination', () => {
+  it('should render successfully', () => {
+    render(<ButtonPagination />);
+  });
+});
